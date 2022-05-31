c4dd52bf53e0e431aea2604825ddb424f87256cc
Mokshith Gowda <71340740+MokshithGowda13@users.noreply.github.com>
2022-05-31 22:34:07 +0530

    31-05-2022 10.30PM

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
             <div class="container">
                 <div class="signin-content">
                     <div class="signin-image">
-                        <figure><img src="./login/images/signin-image.jpg" alt="sing up image"></figure>
+                        <figure><img src="./login/images/login.jpeg" alt="sing up image"></figure>
                         <a href="./register.php" class="signup-image-link">Create an account</a>
                     </div>
 
